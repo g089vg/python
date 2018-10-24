0a15ddfc4552e3604c9b2bffe9f32a9b85bdda72
g089vg <33533762+g089vg@users.noreply.github.com>
2018-10-24 09:04:31 +0900

    Update load_dataset.py

--- load_dataset.py
+++ load_dataset.py
@@ -9,6 +9,7 @@ from PIL import Image
 import numpy as np
 import glob
 import os
+import os.path
 from PIL import ImageFile
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 from random import random
@@ -89,7 +90,55 @@ def Load_Dataset(Folder_Name, mode,channels, num):
         print(len(imageData))
         print(threshold)
         return train,test    
+  
+    if channels == 13: #領域分割結果を合わせて入力
+        imageData = []
+        labelData = []
+        for pathAndLabel in allData:
+            load_num = load_num + 1
+            load_rate = load_rate+1.0
+            if load_num%1000 ==0 : print ("load_rate={:.4}%\n\n".format(load_rate/number*100))
+#            print(pathAndLabel[0])
+            img = np.array(Image.open(pathAndLabel[0]).convert('L') )
             
+            
+            if os.path.isfile("./data/seg_hall_inf/"+pathAndLabel[0]) == True:
+                seg_img1 = np.array(Image.open("./data/seg_hall_inf/"+pathAndLabel[0]))
+            else :
+                seg_img1 = np.array(np.full((50,50,), 0, dtype=np.uint8))
+
+            if os.path.isfile("./data/seg_shadow_inf/"+pathAndLabel[0]) == True:
+                seg_img2 = np.array(Image.open("./data/seg_shadow_inf/"+pathAndLabel[0]))
+            else :
+                seg_img2 = np.array(np.full((50,50,), 0, dtype=np.uint8))
+                
+            if os.path.isfile("./data/seg_hyouzi_inf/"+pathAndLabel[0]) == True:
+                seg_img3 = np.array(Image.open("./data/seg_hyouzi_inf/"+pathAndLabel[0]))
+            else :
+                seg_img3 = np.array(np.full((50,50,), 0, dtype=np.uint8))
+                
+
+    
+            grayImgData = np.asarray(np.float32(img)/255.0)
+            seg1  = np.asarray(np.float32(seg_img1)/255.0)
+            seg2  = np.asarray(np.float32(seg_img2)/255.0)
+            seg3  = np.asarray(np.float32(seg_img3)/255.0)
+            
+            
+            imgData = np.asarray([grayImgData,seg1,seg2,seg3])
+            imageData.append(imgData)
+            labelData.append(np.int32(pathAndLabel[1]))
+
+            
+        threshold = len(imageData) -100 
+        train = tuple_dataset.TupleDataset(imageData[0:threshold], labelData[0:threshold])
+        test  = tuple_dataset.TupleDataset(imageData[threshold:],  labelData[threshold:])
+        np.save('./np_data/image_95-5_'+num+mode+'.npy', imageData)
+        np.save('./np_data/label_95-5_'+num+mode+'.npy', labelData)
+        print(len(imageData))
+        print(threshold)
+        return train,test    
+          
     if channels == 3:
         imageData = []
         labelData = []
@@ -209,13 +258,20 @@ def Load_Dataset(Folder_Name, mode,channels, num):
 #            print(pathAndLabel[0])
 
             if int(pathAndLabel[1]) == 1 :
-                img = np.array(Image.open(pathAndLabel[0]).convert('L') )
-                grayImgData = np.asarray(np.float32(img)/255.0)
-                imgData = np.asarray([grayImgData])
+#                img = np.array(Image.open(pathAndLabel[0]).convert('L') )
+                img = Image.open(pathAndLabel[0])
+                r,g,b = img.split()
+                rImgData = np.asarray(np.float32(r)/255.0)
+                gImgData = np.asarray(np.float32(g)/255.0)
+                bImgData = np.asarray(np.float32(b)/255.0)
+                imgData = np.asarray([rImgData, gImgData, bImgData])
+#                grayImgData = np.asarray(np.float32(img)/255.0)
+#                imgData = np.asarray([grayImgData])
                 imageData.append(imgData)
                 labelData.append(np.int32(pathAndLabel[1]))
 
             
 
-        train = tuple_dataset.TupleDataset(imageData)
-        return imageData     
+        train = np.array(imageData)
+        print(train.shape)
+        return train     
