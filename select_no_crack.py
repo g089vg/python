0b7e0f517b38d42bdcdefbf2ebb09d7b50851018
g089vg <33533762+g089vg@users.noreply.github.com>
2018-11-07 16:25:59 +0900

    Update select_no_crack.py

--- select_no_crack.py
+++ select_no_crack.py
@@ -6,9 +6,19 @@ Created on Thu Oct 18 12:01:20 2018
 """
 import os
 import cv2
+from random import random as rand
 file_list = os.listdir(r"./data/half_data/")
 mode = "rgb"
 channels = 3
+
+def Delete_File (top = '削除したいディレクトリ'):     
+  for root, dirs, files in os.walk(top, topdown=False):
+      for name in files:
+          os.remove(os.path.join(root, name))
+      for name in dirs:
+          os.rmdir(os.path.join(root, name))
+          
+Delete_File("./data/NO_CRACK_DATA/")
 for file_name in file_list:
     root, ext = os.path.splitext(file_name)
     if ext == u'.bmp':
@@ -26,6 +36,9 @@ for file_name in file_list:
         for file_name2 in file_list2:
             root, ext = os.path.splitext(file_name2)
             if ext == u'.bmp':
-                print("1----"+data_dir_path2+file_name2)
-                img = cv2.imread(data_dir_path2+file_name2)
-                cv2.imwrite("./data/NO_CRACK_DATA/"+file_name2,img)
+                
+                if os.path.isfile("./data/seg_hyouzi_batch/"+file_name2) == False:
+                        
+                    print("1----"+data_dir_path2+file_name2)
+                    img = cv2.imread(data_dir_path2+file_name2)
+                    cv2.imwrite("./data/NO_CRACK_DATA/"+file_name2,img)
