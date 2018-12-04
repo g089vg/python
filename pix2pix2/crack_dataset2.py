3330291b09afe2bbe50374322e1e5ec18a959b0f
g089vg <33533762+g089vg@users.noreply.github.com>
2018-12-04 09:55:51 +0900

    Update crack_dataset2.py

--- pix2pix2/crack_dataset2.py
+++ pix2pix2/crack_dataset2.py
@@ -1,123 +1,73 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  4 09:38:24 2018
 
-# python train_facade.py -g 0 -i ./facade/base --out result_facade --snapshot_interval 10000
+@author: g089v
+"""
 
-from __future__ import print_function
-import argparse
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 26 09:03:03 2018
+
+@author: g089v
+"""
+
 import os
+import cv2
+import numpy
+from PIL import Image
+import six
 
-import chainer
-from chainer import training
-from chainer.training import extensions
-from chainer import serializers
+import numpy as np
 
-from net import Discriminator
-from net import Encoder
-from net import Decoder
-from updater import FacadeUpdater
+from io import BytesIO
+import os
+import pickle
+import json
+import numpy as np
 
-from crack_dataset2 import CrackDataset
-from facade_dataset import FacadeDataset
-from facade_visualizer import out_image
+import skimage.io as io
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='chainer implementation of pix2pix')
-    parser.add_argument('--batchsize', '-b', type=int, default=1,
-                        help='Number of images in each mini-batch')
-    parser.add_argument('--epoch', '-e', type=int, default=200,
-                        help='Number of sweeps over the dataset to train')
-    parser.add_argument('--gpu', '-g', type=int, default=0,
-                        help='GPU ID (negative value indicates CPU)')
-    parser.add_argument('--dataset', '-i', default='./facade/base',
-                        help='Directory of image files.')
-    parser.add_argument('--out', '-o', default='result',
-                        help='Directory to output the result')
-    parser.add_argument('--resume', '-r', default='',
-                        help='Resume the training from snapshot')
-    parser.add_argument('--seed', type=int, default=0,
-                        help='Random seed')
-    parser.add_argument('--snapshot_interval', type=int, default=1000,
-                        help='Interval of snapshot')
-    parser.add_argument('--display_interval', type=int, default=100,
-                        help='Interval of displaying log to console')
-    args = parser.parse_args()
+from chainer.dataset import dataset_mixin
 
-    print('GPU: {}'.format(args.gpu))
-    print('# Minibatch-size: {}'.format(args.batchsize))
-    print('# epoch: {}'.format(args.epoch))
-    print('')
-
-    # Set up a neural network to train
-    enc = Encoder(in_ch=5)
-    dec = Decoder(out_ch=3)
-    dis = Discriminator(in_ch=5, out_ch=3)
+# download `BASE` dataset from http://cmp.felk.cvut.cz/~tylecr1/facade/
+class CrackDataset(dataset_mixin.DatasetMixin):
+    def __init__(self, dataDir='./crack', data_range=(1,88)):
+        print("load dataset start")
+        print("    from: %s"%dataDir)
+        print("    range: [%d, %d)"%(data_range[0], data_range[1]))
+        self.dataDir = dataDir
+        self.dataset = []
+        for i in range(data_range[0],data_range[1]):
+            img = Image.open(dataDir+"/cmp_b%04d.jpg"%i)
+            label = Image.open(dataDir+"/cmp_b%04d.png"%i)
+            w,h = img.size
+            r = 350 / float(min(w,h))
+            # resize images so that min(w, h) == 286
+            img = img.resize((int(r*w), int(r*h)), Image.BILINEAR)
+            label = label.resize((int(r*w), int(r*h)), Image.NEAREST)
+            
+            img = np.asarray(img).astype("f").transpose(2,0,1)/128.0-1.0
+            label_ = np.asarray(label)-1  # [0, 12)
+            label = np.zeros((5, img.shape[1], img.shape[2])).astype("i")
+            for j in range(5):
+                label[j,:] = label_==j
+            self.dataset.append((img,label))
+        print(len(self.dataset))
+        print("load dataset done")
     
-    if args.gpu >= 0:
-        chainer.cuda.get_device(args.gpu).use()  # Make a specified GPU current
-        enc.to_gpu()  # Copy the model to the GPU
-        dec.to_gpu()
-        dis.to_gpu()
+    def __len__(self):
+        return len(self.dataset)
 
-    # Setup an optimizer
-    def make_optimizer(model, alpha=0.0002, beta1=0.5):
-        optimizer = chainer.optimizers.Adam(alpha=alpha, beta1=beta1)
-        optimizer.setup(model)
-        optimizer.add_hook(chainer.optimizer.WeightDecay(0.00001), 'hook_dec')
-        return optimizer
-    opt_enc = make_optimizer(enc)
-    opt_dec = make_optimizer(dec)
-    opt_dis = make_optimizer(dis)
-
-    train_d = CrackDataset(args.dataset, data_range=(1,10))
-    test_d = CrackDataset(args.dataset, data_range=(11,15))
-#    train_d = FacadeDataset(args.dataset, data_range=(1,300))
-#    test_d = FacadeDataset(args.dataset, data_range=(1,379))
-#    print(train_d.shape,test_d.shape)
-    #train_iter = chainer.iterators.MultiprocessIterator(train_d, args.batchsize, n_processes=4)
-    #test_iter = chainer.iterators.MultiprocessIterator(test_d, args.batchsize, n_processes=4)
-    train_iter = chainer.iterators.SerialIterator(train_d, args.batchsize)
-    test_iter = chainer.iterators.SerialIterator(test_d, args.batchsize)
-
-    # Set up a trainer
-    updater = FacadeUpdater(
-        models=(enc, dec, dis),
-        iterator={
-            'main': train_iter,
-            'test': test_iter},
-        optimizer={
-            'enc': opt_enc, 'dec': opt_dec, 
-            'dis': opt_dis},
-        device=args.gpu)
-    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
-
-    snapshot_interval = (args.snapshot_interval, 'iteration')
-    display_interval = (args.display_interval, 'iteration')
-    trainer.extend(extensions.snapshot(
-        filename='snapshot_iter_{.updater.iteration}.npz'),
-                   trigger=snapshot_interval)
-    trainer.extend(extensions.snapshot_object(
-        enc, 'enc_iter_{.updater.iteration}.npz'), trigger=snapshot_interval)
-    trainer.extend(extensions.snapshot_object(
-        dec, 'dec_iter_{.updater.iteration}.npz'), trigger=snapshot_interval)
-    trainer.extend(extensions.snapshot_object(
-        dis, 'dis_iter_{.updater.iteration}.npz'), trigger=snapshot_interval)
-    trainer.extend(extensions.LogReport(trigger=display_interval))
-    trainer.extend(extensions.PrintReport([
-        'epoch', 'iteration', 'enc/loss', 'dec/loss', 'dis/loss',
-    ]), trigger=display_interval)
-    trainer.extend(extensions.ProgressBar(update_interval=10))
-    trainer.extend(
-        out_image(
-            updater, enc, dec,
-            5, 5, args.seed, args.out),
-        trigger=snapshot_interval)
-
-    if args.resume:
-        # Resume from a snapshot
-        chainer.serializers.load_npz(args.resume, trainer)
-
-    # Run the training
-    trainer.run()
-
-
-   
+    # return (label, img)
+    def get_example(self, i, crop_width=256):
+        _,h,w = self.dataset[i][0].shape
+        x_l = np.random.randint(0,w-crop_width)
+        x_r = x_l+crop_width
+        y_l = np.random.randint(0,h-crop_width)
+        y_r = y_l+crop_width
+        return self.dataset[i][1][:,y_l:y_r,x_l:x_r], self.dataset[i][0][:,y_l:y_r,x_l:x_r]       
+            
+            
+            
+            
